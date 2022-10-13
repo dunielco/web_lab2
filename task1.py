472750dd9728b3453dbaeaa3bdd1d0ef8055b7fe
dunielco <114190608+dunielco@users.noreply.github.com>
2022-10-13 20:06:46 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+def palindrome(num):
+    num = int(num)
+    copy_num=num
+    check = 0
+    while(num!=0):
+        digit = num%10
+        result = result*10 + digit
+        num=int(num/10)
+    return check == copy_num
+
+print(palindrome(input()))
+
+#   by dunielco
