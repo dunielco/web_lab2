d53a63cc88eba62075d64d62955a98252f36a814
dunielco <114190608+dunielco@users.noreply.github.com>
2022-10-13 20:58:57 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,11 +1,11 @@
 def palindrome(num):
     num = int(num)
-    copy_num=num
+    copy_num = num
     check = 0
-    while(num!=0):
-        digit = num%10
-        result = result*10 + digit
-        num=int(num/10)
+    while(num != 0):
+        digit = num % 10
+        check = check * 10 + digit
+        num = int(num / 10)
     return check == copy_num
 
 print(palindrome(input()))
