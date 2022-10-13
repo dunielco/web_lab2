4ed4ea63f32aa69dbc8a9ab23509ddb75e912946
dunielco <114190608+dunielco@users.noreply.github.com>
2022-10-13 23:41:48 +0300

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,9 @@
+import functools    #Библиотека для декоратора
+
+@functools.cache    #Кэширование
+def factorial(num):
+    return num * factorial(num-1) if num else 1 #Вычисление факториала
+
+#Ввод и вызов функции
+num = int(input('Число: '))
+print(factorial(num))
